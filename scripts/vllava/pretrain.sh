a8e3f5ac01d6bfe0b5b579df33612894fce36625
sennnnn <cyanlaser@stu.pku.edu.cn>
2024-10-13 15:41:41 +0800

    Update scripts.

--- scripts/vllava/pretrain.sh
+++ scripts/vllava/pretrain.sh
@@ -4,8 +4,8 @@
 ARG_WORLD_SIZE=${1:-1}
 ARG_NPROC_PER_NODE=${2:-8}
 ARG_MASTER_ADDR="127.0.0.1"
-ARG_MASTER_PORT=16666
-ARG_RANK=0
+ARG_MASTER_PORT=16667
+ARG_RANK=${3:-0}
 
 # Multiple conditions
 if [ ! -n "$WORLD_SIZE" ] || [ ! -n "$NPROC_PER_NODE" ]; then
@@ -28,13 +28,13 @@ GRADIENT_ACCUMULATION_STEPS=$[$GLOBAL_BATCH_SIZE/($WORLD_SIZE*$NPROC_PER_NODE*$L
 
 # Log Arguments
 export TRANSFORMERS_OFFLINE=1
-export WANDB_PROJECT=videollama2_vllava
-RUN_NAME=videollama2_vllava
+export WANDB_PROJECT=videollama2_vllava/v1.0_refine_modest
+RUN_NAME=tcv35_7b
 DATA_DIR=datasets
 OUTP_DIR=work_dirs
 
 torchrun --nnodes $WORLD_SIZE \
-    --nproc_per_node $NPROC_PER_NODE  \
+    --nproc_per_node $NPROC_PER_NODE \
     --master_addr=$MASTER_ADDR \
     --master_port=$MASTER_PORT \
     --node_rank $RANK \
@@ -42,14 +42,12 @@ torchrun --nnodes $WORLD_SIZE \
     --deepspeed scripts/zero3.json \
     --version plain \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --mm_projector_type stc_connector \
+    --mm_projector_type stc_connector_v35 \
     --tune_mm_mlp_adapter True \
     --model_name_or_path mistralai/Mistral-7B-Instruct-v0.2 \
     --data_path   ${DATA_DIR}/videollava_pt/valley_llavaimage.json \
     --data_folder ${DATA_DIR}/videollava_pt/ \
     --mm_vision_select_layer -2 \
-    --mm_use_im_start_end False \
-    --mm_use_im_patch_token False \
     --num_frames 8 \
     --bf16 True \
     --tf32 True \
@@ -59,7 +57,6 @@ torchrun --nnodes $WORLD_SIZE \
     --per_device_train_batch_size $LOCAL_BATCH_SIZE \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS \
-    --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 500 \
     --save_total_limit 99 \
@@ -70,7 +67,6 @@ torchrun --nnodes $WORLD_SIZE \
     --logging_steps 1 \
     --model_max_length 2048 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
-    --lazy_preprocess True \
+    --dataloader_num_workers 16 \
     --report_to tensorboard \
     --run_name $RUN_NAME \
